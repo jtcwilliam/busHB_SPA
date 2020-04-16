6eb5593aa597a96966bf5416059f2c13b981e799
William Ferreira <jtcwilliam@gmail.com>
2020-04-16 13:21:43 -0300

    Alterado o modelo do Cabecalho

--- bus-hb/src/App.vue
+++ bus-hb/src/App.vue
@@ -1,40 +1,74 @@
-<template >
-   <div id="app" >
-     <h1>asdas</h1>
-     
-   </div>
- 
- 
-</template>
+    <template >
+      <div id="app" >
+          <div class="cabecalho">
+              <div class="container"> 
+                <div class="row">
+                  <div class="col-sm-7   tituloCabecalho">
+                      Hamburgada do Bem - Reserva de Poltrona
+                  </div>
+                  
+                  <div class="col-sm-5   ">
+                    <img src="./assets/logo.png" />
+                  </div>
+                </div>
+              </div>
 
-<script>
-export default {
-  name: 'App',
+          </div>
+      </div>
+    
+    
+    </template>
 
-  data() {
-    return {
-      color: 'black'
+    <script>
+    export default {
+      name: 'App',
+
+      data() {
+        return {
+          color: 'black'
+        }
+      }
     }
-  }
-}
-</script>
+    </script>
 
-<style>
+    <style>
 
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
+    html, body {
+      height: 100%;
+      margin: 0;
+      padding: 0;
+    }
 
-#app {
-  /*
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: whitesmoke;
-  margin-top: 60px;
-  */
-}
-</style>
+      @font-face { font-family: Roger-Serif; src:url('assets/fonts/roger-serif_bold-webfont.woff'); }
+
+
+    .tituloCabecalho{
+      padding-top: 5vh;
+      font-family: 'Roger-Serif';
+      font-size: x-large;
+      height: 10vh;
+    }
+
+
+    .cabecalho{
+
+      background-color: white;
+      color: #4F8DB7;
+      
+      ;
+
+    }
+
+
+
+    #app {
+      /*
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: whitesmoke;
+      margin-top: 60px;
+      */
+    }
+    </style>
--- bus-hb/src/main.js
+++ bus-hb/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import rogerserif_bold  from './assets/fonts/roger-serif_bold-webfont.woff'
+
+  
 
 Vue.config.productionTip = false
 
